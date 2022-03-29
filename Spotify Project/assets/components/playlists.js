bc5fdb7fbc74f841b69e09a403f4ef865d8d27f7
E202859Z <gabriel.comte@etu.univ-nantes.fr>
2022-03-29 21:59:15 +0200

    avancement

---
+++ Spotify Project/assets/components/playlists.js
@@ -0,0 +1,21 @@
+import useSpotifyApi from "../spotify.js";
+
+Vue.component('playlists',{
+    props: ['imgSrcp','titlep'],
+    template: ` <div class="container">    
+                    <div class="col">
+                        <img class="img-fluid" v-bind:src=imgSrcp>
+                        <br/>
+                        <p class="center"><strong> {{titlep}} </strong></p>
+                    </div>
+                </div>`,
+
+    data: function(){
+        return{
+            
+        }
+    },
+    methods:{
+       
+    }
+})
