ffa6f0698ef68b65714f4fe8e0d589686cd5f95e
E208681C <cyprien.crusson@etu.univ-nantes.fr>
2022-03-30 11:11:19 +0200

    ajout de l'intégrateur

--- Spotify Project/assets/components/playlists.js
+++ Spotify Project/assets/components/playlists.js
@@ -1,23 +1,27 @@
 import useSpotifyApi from "../spotify.js";
 
 Vue.component('playlists',{
-    props: ['imgSrcp','titlep'],
+    props: ['imgSrcp','titlep', 'url'],
     template: ` <div class="container">
-                <div class="row">    
-                    <div class="col">
-                            <img class="img-fluid card-img-top" v-bind:src=imgSrcp>
-                            <br/>
-                            <p class="card-title center" style="color: black !important"> {{titlep}} </p>
+                    <div class="row">   
+                        <div class="col">
+                                <img class="img-fluid card-img-top" v-bind:src=imgSrcp>
+                        </div>
+
+                        <div class="col">
+                        <p>{{titlep}}</p>
+                        <iframe v-bind:src=test width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
                         </div>
                     </div>    
+                    
                 </div>`,
 
     data: function(){
         return{
-            
+            test:"https://open.spotify.com/embed/playlist/"+this.url+"?utm_source=generator"
         }
     },
     methods:{
        
-    }
+    },
 })
