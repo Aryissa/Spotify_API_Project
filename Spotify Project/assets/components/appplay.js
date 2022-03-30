e13fd2bbd44744913201c8424550b1ac003443a5
E202859Z <gabriel.comte@etu.univ-nantes.fr>
2022-03-30 11:00:44 +0200

    avancement

--- Spotify Project/assets/components/appplay.js
+++ Spotify Project/assets/components/appplay.js
@@ -2,7 +2,6 @@ import useSpotifyApi from "../spotify.js";
 
 Vue.component('appplay',{
     template:`  <div>
-                    <input type="submit"  @click="getlist()">
                     <div class="container">
                         <div class="row justify-content-center">
                             <div v-for="playlist in listPlaylist"  v-if="playlist.images[0] != undefined" class="col-2">            
@@ -17,9 +16,11 @@ Vue.component('appplay',{
         }
     },
     methods: {
-        getlist: async function(){
-            let list=await useSpotifyApi.getPlaylistsUser();
-            this.listPlaylist=list.items
-        }
+        
+    },
+    async mounted() {
+        let list=await useSpotifyApi.getPlaylistsUser();
+        this.listPlaylist=list.items
+        var elems = document.getElementById('appplay').querySelector('.container');
     }
 })
